64f6fed05c0a4a454d485461ed47fd39f277400f
st0q <tssk713@gmail.com>
2024-02-07 09:45:52 +0900

    add controller generator

--- plopfile.js
+++ plopfile.js
@@ -1,9 +1,20 @@
 export default function (plop) {
-    // create your generators here
-    plop.setGenerator('basics', {
-        description: 'this is a skeleton plopfile',
-        prompts: [], // array of inquirer prompts
-        actions: []  // array of actions
-    });
-};
-
+  // controller generator
+  plop.setGenerator('controller', {
+    description: 'application controller logic',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'controller name please',
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: 'src/{{name}}.js',
+        templateFile: 'plop-templates/controller.hbs',
+      },
+    ],
+  });
+}
