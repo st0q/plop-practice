7116e6f12e410c84a16088b2b7d291d509036780
st0q <tssk713@gmail.com>
2024-02-07 09:18:32 +0900

    first commit

---
+++ plopfile.js
@@ -0,0 +1,9 @@
+export default function (plop) {
+    // create your generators here
+    plop.setGenerator('basics', {
+        description: 'this is a skeleton plopfile',
+        prompts: [], // array of inquirer prompts
+        actions: []  // array of actions
+    });
+};
+
